28cc513682425d49f8567af1e6becf5366235e73
Tshego <219001235@mycput.ac.za>
2023-03-10 20:19:27 +0200

    First Commit Base code created

---
+++ src/main/java/za/ac/cput/domain/Employee.java
@@ -0,0 +1,45 @@
+package za.ac.cput.domain;
+
+public class Employee {
+    private int empNumber;
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+    public Employee(){
+
+    }
+
+    public Employee(int empNumber, String firstName, String lastName, double salary) {
+        this.empNumber = empNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getEmpNumber() {
+        return empNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empNumber=" + empNumber +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
